178143629e598bcfcda44d7aca41b2ef3e7104d9
Xeno <68283330+xenopeltis1@users.noreply.github.com>
2020-11-23 15:23:23 +0300

    Update heap.py

--- heap.py
+++ heap.py
@@ -1,31 +1,36 @@
-def heapify(arr, n, i):
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
+def agac_olustur(listt, lenn, maxel):
+    root = maxel
 
-    if l < n and arr[largest] < arr[l]:
-        largest = l
+    #root elemanın solundaki değer
+    l = 2 * maxel + 1
 
-    if r < n and arr[largest] < arr[r]:
-        largest = r
+    #root elemanın sağındaki değer
+    r = 2 * maxel + 2
 
+    #root eleman ile karşılaştırma
+    if l < lenn and listt[root] < listt[l]:
+        root = l
 
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
+    #root eleman ile karşılaştırma
+    if r < lenn and listt[root] < listt[r]:
+        root = r
+
+    #eğer ağac yanlışsa tekrar düzenlenir
+    if root != maxel:
+        listt[maxel], listt[root] = listt[root], listt[maxel]
+
+        agac_olustur(listt, lenn, root)
+
+#şimdi ise sıralama
+def heapSort(listt):
+    lenn = len(listt)
  
-
-        heapify(arr, n, largest)
-
-
-def heapSort(arr):
-    n = len(arr)
+    for i in range(lenn//2-1, -1, -1):
+        agac_olustur(listt, lenn, i)
  
-    for i in range(n//2 - 1, -1, -1):
-        heapify(arr, n, i)
- 
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
+    for i in range(lenn-1, 0, -1):
+        listt[i], listt[0] = listt[0], arr[i]
+        agac_olustur(listt, i, 0)
     
     return arr
 
