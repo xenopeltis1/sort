b64cf4b47fea22baf57185797f1321922c97da1d
Xeno <68283330+xenopeltis1@users.noreply.github.com>
2020-11-23 15:10:42 +0300

    Update selection.py

--- selection.py
+++ selection.py
@@ -1,16 +1,31 @@
+#fonskiyon oluşturup sıralanacak listemizi parametre olarak alıyoruz
 def selection(liste):
     
+    #listenin uzunluğu kadar dönecek bir for döngüsü oluşturuyoruz
     for i in range(len(liste)):
-        min_idx = i
-        for _ in range(i+1, len(liste)):
-            if liste[min_idx] > liste[_]:
-                min_idx = _
-    
-        liste[i], liste[min_idx] = liste[min_idx], liste[i]
 
+        #minimum elemanımızı belirliyoruz
+        enkucuk = i
+
+        #sıralanmamış alt dizimizin içinde dönüyoruz. i+1 bu işe yarıyor.
+        for _ in range(i+1, len(liste)):
+
+            #en küçük elemanımızı bulup başa yerleştireceğiz
+            if liste[enkucuk] > liste[_]:
+                enkucuk = _
+
+        #yer değiştirme işlemi
+        liste[i], liste[enkucuk] = liste[enkucuk], liste[i]
+
+    #girilen listemizi döndürüyoruz
     return liste
 
+#listemi oluşturdum
 lism = [65, 51, 23, 48]
 print(lism)
-lims = selection(lism)
+
+#fonskiyonu çağırıp döndürdüğü veriyi lism değişkenine atadım
+lism = selection(lism)
 print(lism)
+
+
