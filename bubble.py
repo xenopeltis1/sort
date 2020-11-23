72c5c67376d7a70d58f0b4ccf92a5c1ea03fff03
Xeno <68283330+xenopeltis1@users.noreply.github.com>
2020-11-23 15:13:56 +0300

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,15 +1,30 @@
+#fonskiyon oluşturup listemi aldım
 def BubbleSort(liste):
+
+    #listemin uzunluğunu aldım -> len()
     lenn = len(liste)
 
+    #geçişleri belirleyecek döngüm
     for _ in range(lenn):
+
+        #tüm yan yana olan ikilileri gezmeme yarayacak döngüm. lenn-_-1 yaparak sıralanmış elemanlara tekrar gitmemeyi sağladım. Böylece işlem hızlanacak
         for __ in range(0, lenn-_-1):
-            if list[__]>list[__+1]:
-                list[__], list[__+1] = list[__+1], list[__]
+
+            #sıralamada bir yanlışlık var ise...
+            if liste[__]>liste[__+1]:
+
+                #...elemanların yerlerini değiştir.
+                liste[__], liste[__+1] = liste[__+1], liste[__]
             print(list)
+
+    #listeyi döndür
     return list
 
+#listemi verdim
 listem = [1,5,9,2,7]
 
 print(listem)
+
+#fonksiyonu çağırıp listem değişkenine eşitledim
 listem = BubbleSort(listem)
 print(listem)
